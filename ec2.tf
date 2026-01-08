547e8e354e250df010de0123bf3160eac4df2687
bludwolf69 <bludwolf@bellsouth.net>
2026-01-08 17:27:47 +0000

    ec2 security groups

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
   ami           = var.jk-ami
   instance_type = var.vm-size
+  
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  
   tags = {
     Name = var.vm-Name
   }
